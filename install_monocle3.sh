7bc6c73f35bd6295e728f7712ba9e5bf60ca8c01
nh3 <nh3@users.noreply.github.com>
2019-03-31 19:17:38 +0100

    Make monocle3 installation work
    Squashed commit of the following:
    
    commit f6cab02b22517b8878f7f2d98f95a47648cf130f
    Author: nh3 <nh3@users.noreply.github.com>
    Date:   Sun Mar 31 19:16:46 2019 +0100
    
        Remove redundant check for monocle installation
    
    commit 54030f6d1caae188709b2cde2e6637e3d21170fd
    Author: nh3 <nh3@users.noreply.github.com>
    Date:   Sun Mar 31 18:37:50 2019 +0100
    
        Make use of cached installation
    
    commit b870b4da398db93f284e966e577e21a41357dd50
    Author: nh3 <nh3@users.noreply.github.com>
    Date:   Sun Mar 31 18:32:14 2019 +0100
    
        Make sure PATH is exported
    
    commit b36d12f9fa1a056143154b3391059718c0cd371f
    Author: nh3 <nh3@users.noreply.github.com>
    Date:   Sun Mar 31 18:14:27 2019 +0100
    
        Consolidate installation into a bash scripts
    
    commit 681cbaf9963345f1619bfa1c728d6b2d5c7f7d8c
    Author: nh3 <nh3@users.noreply.github.com>
    Date:   Sun Mar 31 16:50:19 2019 +0100
    
        Install udunits2 via conda instead of apt-get
    
    commit 5c2714d3d40c095bbae02702093c42250bbb7d7e
    Author: nh3 <nh3@users.noreply.github.com>
    Date:   Sun Mar 31 12:20:00 2019 +0100
    
        Install libgdal2 via conda instead of apt-get install libgdal1
    
    commit e782de0181ad65b1a0d2e3b866090221640334b7
    Author: nh3 <nh3@users.noreply.github.com>
    Date:   Sat Mar 30 03:41:08 2019 +0000
    
        Add libgdal1-dev
    
    commit 3c70c3b4d227b3d60d8001b9716d05043dc574cb
    Author: nh3 <nh3@users.noreply.github.com>
    Date:   Sat Mar 30 03:20:34 2019 +0000
    
        Fix typo
    
    commit ffeeb64ab7e481883a962a2e2929740f85763a00
    Author: nh3 <nh3@users.noreply.github.com>
    Date:   Sat Mar 30 03:08:05 2019 +0000
    
        Install r-sf from source
    
    commit 5cf0ae7601bb322f7c568eac56d2c3120b252f15
    Author: nh3 <nh3@users.noreply.github.com>
    Date:   Sat Mar 30 02:46:24 2019 +0000
    
        Remove r-sf
    
    commit bc09c59a2012a3740fdf75b6cc87f430bd6af81a
    Author: nh3 <nh3@users.noreply.github.com>
    Date:   Fri Mar 29 20:08:48 2019 +0000
    
        Fix typo
    
    commit 27ad98ee737df5c465b6eed01439ad8b2f68f700
    Author: nh3 <nh3@users.noreply.github.com>
    Date:   Fri Mar 29 20:00:18 2019 +0000
    
        Dependency tweaks in travis
    
    commit 7b6ea9015421b96a9899314b2b5abc472e77d0b8
    Author: Ni Huang <nh3@mib112657i.internal.sanger.ac.uk>
    Date:   Fri Mar 29 17:35:34 2019 +0000
    
        Add additional dependencies
    
    commit 46b9735ae3a3549397df67b3135a9d83b1f07148
    Author: Ni Huang <nh3@mib112657i.internal.sanger.ac.uk>
    Date:   Fri Mar 29 17:11:27 2019 +0000
    
        Point to tar and gzip by env variable
    
    commit 52b92d761379f304d95633a5f9650ec80c691f55
    Author: Ni Huang <nh3@mib112657i.internal.sanger.ac.uk>
    Date:   Fri Mar 29 16:49:58 2019 +0000
    
        Add r-pkgbuild to list of conda install
    
    commit b7656f8ed72a455c60ed363d5db5f4b91b965ae8
    Author: Ni Huang <nh3@mib112657i.internal.sanger.ac.uk>
    Date:   Fri Mar 29 16:28:22 2019 +0000
    
        tweaks travis

---
+++ install_monocle3.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+env_name="${1:-$TEST_ENV}"
+env_name="${env_name:-monocle3}"
+
+monocle_version="${2:-$MONOCLE_VERSION}"
+monocle_version="${monocle_version:-monocle3_alpha}"
+
+python_version="${3:-$TRAVIS_PYTHON_VERSION}"
+python_version="${python_version:-3.6}"
+
+conda create -n $env_name python=$python_version \
+    bioconductor-monocle=2.10.0 bioconductor-delayedmatrixstats bioconductor-iranges louvain umap-learn \
+    r-class r-classint r-dbi r-devtools r-doparallel r-e1071 r-future r-ggridges r-glmnet r-globals \
+    r-htmltools r-htmlwidgets r-listenv r-lpsolveapi r-pbapply r-plotly r-reticulate r-rgl r-spdep \
+    r-tidyr r-units libgdal udunits2
+
+conda remove -q -n $env_name r-ddrtree bioconductor-monocle
+
+source activate $env_name
+
+Rscript -e 'Sys.setenv(TAR="/bin/tar", R_GZIPCMD="/bin/gzip", R_BZIPCMD="/bin/bzip2")' \
+        -e 'install.packages("pbmcapply", repos="https://cran.ma.imperial.ac.uk")' \
+        -e 'devtools::install_github("cole-trapnell-lab/DDRTree", ref="simple-ppt-like", upgrade="never")' \
+        -e 'devtools::install_github("cole-trapnell-lab/L1-graph", upgrade="never")' \
+        -e 'devtools::install_github("cole-trapnell-lab/monocle-release", ref="'$monocle_version'", upgrade="never")'

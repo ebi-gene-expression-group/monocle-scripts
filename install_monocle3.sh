ee3ff1450badb43fc3f9450a73bf2d71c0a6c136
nh3 <nh3@users.noreply.github.com>
2019-04-10 16:03:27 +0100

    Making install script more robust

--- install_monocle3.sh
+++ install_monocle3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-env_name="${1:-$TEST_ENV}"
+env_name="${1:-$CONDA_DEFAULT_ENV}"
 env_name="${env_name:-monocle3}"
 
 monocle_version="${2:-$MONOCLE_VERSION}"
@@ -9,13 +9,19 @@ monocle_version="${monocle_version:-monocle3_alpha}"
 python_version="${3:-$TRAVIS_PYTHON_VERSION}"
 python_version="${python_version:-3.6}"
 
+if [[ $(conda env list | grep -v '#' | awk -v env=$env_name '$1==env') ]]; then
+    cmd=install
+else
+    cmd=create
+fi
+
 conda config --add channels defaults
 conda config --add channels bioconda
 conda config --add channels conda-forge
 
 conda config --set always_yes yes --set changeps1 no --set channel_priority strict
 
-conda create -q -n $env_name python=$python_version \
+conda $cmd -q -n $env_name python=$python_version \
     bioconductor-monocle=2.10.0 bioconductor-delayedmatrixstats bioconductor-iranges louvain umap-learn \
     r-class r-classint r-dbi r-devtools r-doparallel r-e1071 r-future r-ggridges r-glmnet r-globals \
     r-htmltools r-htmlwidgets r-listenv r-lpsolveapi r-pbapply r-plotly r-reticulate r-rgl r-spdep \
@@ -25,8 +31,11 @@ conda remove -q -n $env_name r-ddrtree bioconductor-monocle
 
 source activate $env_name
 
-Rscript -e 'Sys.setenv(TAR="/bin/tar", R_GZIPCMD="/bin/gzip", R_BZIPCMD="/bin/bzip2")' \
-        -e 'install.packages("pbmcapply", repos="https://cran.ma.imperial.ac.uk")' \
+export TAR=$(which tar)
+export R_GZIPCMD=$(which gzip)
+export R_BZIPCMD=$(which bzip2)
+
+Rscript -e 'install.packages("pbmcapply", repos="https://cloud.r-project.org/")' \
         -e 'devtools::install_github("cole-trapnell-lab/DDRTree", ref="simple-ppt-like", upgrade="never")' \
         -e 'devtools::install_github("cole-trapnell-lab/L1-graph", upgrade="never")' \
         -e 'devtools::install_github("cole-trapnell-lab/monocle-release", ref="'$monocle_version'", upgrade="never")'

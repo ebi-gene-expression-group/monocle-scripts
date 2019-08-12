c1c5326fe4882cfdadaabf3861e8168dc4459591
nh3 <nh3@users.noreply.github.com>
2019-08-12 09:47:26 +0100

    Update install script and travis

--- install_monocle3.sh
+++ install_monocle3.sh
@@ -4,10 +4,10 @@ env_name="${1:-$CONDA_DEFAULT_ENV}"
 env_name="${env_name:-monocle3}"
 
 monocle_version="${2:-$MONOCLE_VERSION}"
-monocle_version="${monocle_version:-monocle3_alpha}"
+monocle_version="${monocle_version:-master}"
 
 python_version="${3:-$TRAVIS_PYTHON_VERSION}"
-python_version="${python_version:-3.6}"
+python_version="${python_version:-3}"
 
 if [[ $(conda env list | grep -v '#' | awk -v env=$env_name '$1==env') ]]; then
     cmd=install
@@ -21,13 +21,12 @@ conda config --add channels conda-forge
 
 conda config --set always_yes yes --set changeps1 no --set channel_priority strict
 
-conda $cmd -q -n $env_name python=$python_version \
-    bioconductor-monocle=2.10.0 bioconductor-delayedmatrixstats bioconductor-iranges louvain umap-learn \
-    r-class r-classint r-dbi r-devtools r-doparallel r-e1071 r-future r-ggridges r-glmnet r-globals \
-    r-htmltools r-htmlwidgets r-listenv r-lpsolveapi r-pbapply r-plotly r-reticulate r-rgl r-spdep \
-    r-tidyr r-units libgdal udunits2
-
-conda remove -q -n $env_name r-ddrtree bioconductor-monocle
+conda $cmd -q -n $env_name python=$python_version louvain umap-learn \
+    r-assertthat r-matrix r-matrix.utils r-ggplot2 r-ggrepel r-viridis r-dplyr r-tidyr r-stringr r-reshape2 \
+    r-plotly r-pryr r-base64enc r-hexbin r-crosstalk r-data.table r-codetools \
+    r-devtools r-grr r-htmlwidgets r-pbapply r-pbmcapply r-irlba r-igraph r-rhpcblasctl r-rtsne r-lmtest \
+    r-pheatmap r-proxy r-pscl r-purrr r-rann r-reticulate r-shiny r-slam r-spdep r-speedglm r-uwot \
+    bioconductor-limma bioconductor-singlecellexperiment bioconductor-delayedarray bioconductor-delayedmatrixstats
 
 source activate $env_name
 
@@ -35,7 +34,4 @@ export TAR=$(which tar)
 export R_GZIPCMD=$(which gzip)
 export R_BZIPCMD=$(which bzip2)
 
-Rscript -e 'install.packages("pbmcapply", repos="https://cloud.r-project.org/")' \
-        -e 'devtools::install_github("cole-trapnell-lab/DDRTree", ref="simple-ppt-like", upgrade="never")' \
-        -e 'devtools::install_github("cole-trapnell-lab/L1-graph", upgrade="never")' \
-        -e 'devtools::install_github("cole-trapnell-lab/monocle-release", ref="'$monocle_version'", upgrade="never")'
+Rscript -e 'devtools::install_github("cole-trapnell-lab/monocle3", ref="'$monocle_version'", upgrade="never")'
